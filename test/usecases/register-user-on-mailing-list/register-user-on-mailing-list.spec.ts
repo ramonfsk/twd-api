aecbc2e25d978b003c8c68110124ffc03759a363
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-17 11:22:11 -0300

    test: should add user with complete data to mailing list

---
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import type { UserData } from '@/usecases/register-user-on-mailing-list/user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const userCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'anyname'
+    // const email = 'any@email.com'
+    // const response = await userCase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('anyname')
+  })
+})
