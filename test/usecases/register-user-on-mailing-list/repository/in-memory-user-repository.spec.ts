61e1b1a55b81a1511584bdcf3db1018d972fa3f4
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-17 13:04:00 -0300

    test: add and find user and checks its existence

--- test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { UserData } from '@/usecases/register-user-on-mailing-list/user-data'
-import { InMemoryUserRepository } from './in-memory-user-repository'
+import { InMemoryUserRepository } from '../../../../src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository'
 
 describe('In memory User repository', () => {
   test('should return null if user not found', async () => {
@@ -7,5 +7,15 @@ describe('In memory User repository', () => {
     const userRepo = new InMemoryUserRepository(users)
     const user = await userRepo.findUserByEmail('any@email.com')
     expect(user).toBeNull()
+  })
+
+  test('should return user if is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'anyname'
+    const email = 'any@email.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@email.com')
+    expect(user.name).toBe('anyname')
   })
 })
