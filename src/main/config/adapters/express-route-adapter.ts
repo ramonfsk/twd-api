2a08363515ddf377bb439c6c2096e231bbff0e7c
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-19 08:57:50 -0300

    feat: express route adapter

---
+++ src/main/config/adapters/express-route-adapter.ts
@@ -0,0 +1,14 @@
+import { RegisterUserController } from '@/web-controllers'
+import { Request, Response } from 'express'
+import { HttpRequest } from '@/web-controllers/ports'
+
+export const adatpRoute = (controller: RegisterUserController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
