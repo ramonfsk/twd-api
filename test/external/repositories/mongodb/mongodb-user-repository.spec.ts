8d5d9bb679f13e06006f7fa36b969d4465399032
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-21 12:32:25 -0300

    test: find all users in mongodb repository

--- test/external/repositories/mongodb/mongodb-user-repository.spec.ts
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -24,4 +24,19 @@ describe('Mongodb User repository', () => {
     await userRepository.add(user)
     expect(await userRepository.exists(user)).toBeTruthy()
   })
+
+  test('find all users should return all added users', async () => {
+    const userRepository = new MongodbUserRepository()
+    await userRepository.add({
+      name: 'First Name',
+      email: 'anyf@mail.com'
+    })
+    await userRepository.add({
+      name: 'Second Name',
+      email: 'anys@mail.com'
+    })
+    const users = await userRepository.findAllUsers()
+    expect(users[0].name).toEqual('First Name')
+    expect(users[1].name).toEqual('Second Name')
+  })
 })
