66f4c12b29a17c23e7666659d091e236aa56e4ee
Ramon Ferreira <ramonfsk@gmail.com>
2023-09-23 23:15:16 -0300

    fix: move server port to .env variable

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,7 @@ import { MongoHelper } from '@/external/repositories/mongodb/helper'
 MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(3000, () => {
+    app.listen(process.env.SERVER_PORT, () => {
       console.log('Server running at http://localhost:3000')
     })
   })
